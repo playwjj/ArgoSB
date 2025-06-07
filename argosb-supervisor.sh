b3294c8d37b66a67f2031d88281f8735a69a867f
Deng Chong <dc2926@gmail.com>
2025-06-07 22:33:41 +0800

    bug fixed

--- argosb-supervisor.sh
+++ argosb-supervisor.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-# 检查是否存在 cloudflared
-if ! command -v cloudflared &> /dev/null; then
-    echo "[ERROR] cloudflared 未安装，请先安装 cloudflared"
-    exit 1
-fi
-
-# 启动 Cloudflare Tunnel
-echo "[cloudflared] Starting tunnel..."
-cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN &
-CLOUDFLARED_PID=$!
-
 echo "[INFO] Starting ArgoSB Script (local)..."
 
 # 映射环境变量为脚本参数，然后执行本地脚本
-vlpt="$PORT" uuid="$UUID" argo="n" bash /app/argosb.sh
-
-echo $TUNNEL_DOMAIN
-
-# 等待 cloudflared 进程
-wait $CLOUDFLARED_PID
+vmpt="$PORT" uuid="$UUID" argo="y" agn="$TUNNEL_DOMAIN" agk="$TUNNEL_TOKEN" bash /app/argosb.sh
 
 tail -f /dev/null
